05932dcb3aa2ea7c4062d5f57f47e965896cf8c6
Shawon Mondol Shibu <Shawonmondol72@gmail.com>
2024-10-05 11:58:53 +0600

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ export default function NavBar() {
       <nav className="fixed z-50 backdrop-blur-md top-0 left-0 w-full bg-[#020202cc] flex text-purple-50 font-bold p-1">
         <a href="#" className={navBtn}>home</a>
         <a href="#about" className={navBtn}>about</a>
-        <a href="#gellery" className={navBtn}>gellery</a>
+        {/*<a href="#gellery" className={navBtn}>gellery</a>*/}
         <a href="#skill" className={navBtn}>skills</a>
         <a href="#project" className={navBtn}>projects</a>
         <a href="#contact" className={navBtn}>contact</a>
