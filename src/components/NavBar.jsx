3989066b3c6a80491ad07820a8f683709810eb40
Shawon Mondol Shibu <Shawonmondol72@gmail.com>
2025-02-23 21:10:49 +0600

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,25 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-
+const navBar = [
+  { title: "home", url: "#" },
+  { title: "about", url: "#about" },
+  { title: "skills", url: "#skill" },
+  { title: "projects", url: "#project" },
+  { title: "contact", url: "#contact" },
+];
 export default function NavBar() {
-  let navBtn = "p-4 uppercase rounded transition-all duration-300 ease-in-out hover:text-purple-700 cursor-pointer hover:rotate-3";
+  let navBtn =
+    "p-4 uppercase rounded transition-all duration-300 ease-in-out hover:text-purple-700 cursor-pointer hover:rotate-3";
   return (
     <>
       <nav className="fixed z-50 backdrop-blur-md top-0 left-0 w-full bg-[#020202cc] flex text-purple-50 font-bold p-1">
-        <a href="#" className={navBtn}>home</a>
-        <a href="#about" className={navBtn}>about</a>
+        {navBar.map((item, i) => (
+          <a key={i} href={item.url} className={navBtn}>
+            {item.title}
+          </a>
+        ))}
+
         {/*<a href="#gellery" className={navBtn}>gellery</a>*/}
-        <a href="#skill" className={navBtn}>skills</a>
-        <a href="#project" className={navBtn}>projects</a>
-        <a href="#contact" className={navBtn}>contact</a>
       </nav>
     </>
   );
